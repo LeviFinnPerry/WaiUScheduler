80e75450f0ed01c41a090e496cbc67624455e659
Perry, Levi <jp247@students.waikato.ac.nz>
2026-04-22 15:13:49 +1200

    Converts event, assessment and studysession tables into a CalendarOccurrence

--- app/src/main/java/com/example/waiuscheduler/ui/calendar/CalendarOccurrence.java
+++ app/src/main/java/com/example/waiuscheduler/ui/calendar/CalendarOccurrence.java
@@ -7,65 +7,80 @@ import com.example.waiuscheduler.database.tables.StudySessionEntity;
 import java.util.Date;
 
 public class CalendarOccurrence {
-    private String type;
-    private final String colour;
-    private Date startDateTime;
-    private Date endDateTime;
+
+    // Constant variables
+    public static final String TYPE_ASSESSMENT = "Assessment";
+    public static final String TYPE_EVENT = "Event";
+    public static final String TYPE_STUDY = "Study Session";
+
+    // Field variables
+    private final String type;
+    private final String title;
+    private final int colour;
+    private final Date startDateTime;
+    private final Date endDateTime;
+    private final Object sourceEntity;  // For dialog actions
 
     /// Converts different events into a uniform calendar object
-    private CalendarOccurrence(String type, Date startDateTime, Date endDateTime) {
+    private CalendarOccurrence(String type, String title, Date start, Date end, Object src) {
         this.type = type;
+        this.title = title;
         this.colour = findColour(type);
-        this.startDateTime = startDateTime;
-        this.endDateTime = endDateTime;
+        this.startDateTime = start;
+        this.endDateTime = end;
+        this.sourceEntity = src;
     }
 
     /// Converts an assessment entity into a calendar object
     /// @param assessment Occurrence of an assessment
     /// @return Calendar object representing the assessment
-    public CalendarOccurrence assessmentToCalendar(AssessmentEntity assessment) {
-        String type = assessment.getType();
-        Date startDateTime = assessment.getDueDate();
-        Date endDateTime = assessment.getDueDate();
-
-        return new CalendarOccurrence(type, startDateTime, endDateTime);
+    public static CalendarOccurrence from(AssessmentEntity assessment) {
+        return new CalendarOccurrence(
+                TYPE_ASSESSMENT,
+                assessment.getTitle(),
+                assessment.getDueDate(),
+                assessment.getDueDate(),
+                assessment
+        );
     }
 
     /// Converts an event entity into a calendar object
     /// @param event Occurrence of an event
     /// @return Calendar object representing the event
-    public CalendarOccurrence eventToCalendar(EventEntity event) {
-        String type = "Event";
-        Date startDateTime = event.getDateTimeStart();
-        Date endDateTime = event.getDateTimeEnd();
-
-        return new CalendarOccurrence(type, startDateTime, endDateTime);
+    public static CalendarOccurrence from(EventEntity event) {
+        return new CalendarOccurrence(
+                TYPE_EVENT,
+                event.getTimetableId_fk(),
+                event.getDateTimeStart(),
+                event.getDateTimeEnd(),
+                event
+        );
     }
 
     /// Converts a study session entity into a calendar object
     /// @param study Occurrence of an study session
     /// @return Calendar object representing the study session
-    public CalendarOccurrence studyToCalendar(StudySessionEntity study) {
-        String type = "Study";
-        Date startDateTime = study.getDateTimeStart();
-        Date endDateTime = study.getDateTimeEnd();
-
-        return new CalendarOccurrence(type, startDateTime, endDateTime);
+    public static CalendarOccurrence from(StudySessionEntity study) {
+        return new CalendarOccurrence(
+                TYPE_STUDY,
+                study.getPaperId_fk().split("-")[0] != null ? study.getPaperId_fk() : "Study Session",
+                study.getDateTimeStart(),
+                study.getDateTimeEnd(),
+                study
+        );
     }
 
-    /// Determines colour for the types of events
+    /// Determines title for the types of events
     /// @param type type of event
-    /// @return colour
-    private String findColour(String type) {
+    /// @return title
+    private static int findColour(String type) {
         // TODO: Include different assessment types and event types
-        if (type.matches("Study")) {
-            return "Blue";
-        } else if (type.matches("Assessment")) {
-            return "Red" ;
-        } else if (type.matches("Lecture")) {
-            return "Green";
+        switch (type) {
+            case TYPE_STUDY: return 0xFF0000FF; // Blue
+            case TYPE_ASSESSMENT: return 0xFFFF0000; // Red
+            case TYPE_EVENT: return 0xFF00FF00; // Green
+            default: return 0xFF757575; // Grey
         }
-        return null;
     }
 
 
@@ -73,27 +88,20 @@ public class CalendarOccurrence {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
 
-    public String getColour() {
-        return colour;
+    public String getTitle() {
+        return title;
     }
 
     public Date getStartDateTime() {
         return startDateTime;
     }
 
-    public void setStartDateTime(Date startDateTime) {
-        this.startDateTime = startDateTime;
-    }
-
     public Date getEndDateTime() {
         return endDateTime;
     }
 
-    public void setEndDateTime(Date endDateTime) {
-        this.endDateTime = endDateTime;
+    public Object getSourceEntity() {
+        return sourceEntity;
     }
 }
