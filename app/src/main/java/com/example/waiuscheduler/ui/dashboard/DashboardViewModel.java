4cf648cc68b9875624ed779ccfb4518118214d37
Perry, Levi <jp247@students.waikato.ac.nz>
2026-05-02 18:44:24 +1200

    View Model handles DashboardRepository

--- app/src/main/java/com/example/waiuscheduler/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/waiuscheduler/ui/dashboard/DashboardViewModel.java
@@ -1,19 +1,37 @@
 package com.example.waiuscheduler.ui.dashboard;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.waiuscheduler.ui.dashboard.extension.DashboardRepository;
+import com.example.waiuscheduler.ui.dashboard.extension.rows.CourseGradeRow;
+import com.example.waiuscheduler.ui.dashboard.extension.rows.StudyHourRow;
+import com.example.waiuscheduler.ui.dashboard.extension.rows.UpcomingAssessments;
+
+import java.util.List;
 
 public class DashboardViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
-    public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+    private final DashboardRepository dashRepository;
+    
+    public DashboardViewModel(@NonNull Application app) {
+        dashRepository = new DashboardRepository(app);
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+    public LiveData<Double> getTotalStudy() { return dashRepository.getTotalStudy(); }
+
+    public LiveData<Double> getAvgGrade() { return dashRepository.getAvgGrade(); }
+
+    public LiveData<Integer> getUpcomingEventCount() { return dashRepository.getUpcomingEventCount(); }
+
+    public LiveData<Integer> getTotalPaperCount() { return dashRepository.getTotalPaperCount(); }
+    public LiveData<List<UpcomingAssessments>> getUpcomingAssessments() { return dashRepository.getUpcomingAssessments(); }
+
+    public LiveData<List<CourseGradeRow>> getGradesByPaper() { return dashRepository.getGradesByPaper(); }
+
+    public LiveData<List<StudyHourRow>> getTotalStudyByPaper() { return dashRepository.getTotalStudyByPaper(); }
+
 }
